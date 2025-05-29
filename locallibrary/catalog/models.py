5d830e8ab8793c436cffcaf1a563fa49d01f1291
Arthur Camargo <avccamargo@inf.ufrgs.br>
2025-05-29 14:33:24 -0300

    Fix langauge typo

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -52,7 +52,7 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre,
                                    help_text="Select a genre for this book")
 
-    language = models.ForeignKey('Langauge', on_delete=models.SET_NULL,
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL,
                                  null=True)
 
     def __str__(self):
